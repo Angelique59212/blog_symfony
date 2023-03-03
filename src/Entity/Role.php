4b5f63e2e98fc8231e81614878e56ecfb8b73e3e
angelika <dehainaut.angelique@orange.fr>
2023-03-03 12:03:39 +0100

    ==> create entity ==> Create migration

---
+++ src/Entity/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoleRepository::class)]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id;
+
+    #[ORM\Column(length: 70)]
+    private ?string $role;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
